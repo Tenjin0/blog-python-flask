bae7efb69b3aefe24538464f556c5a159ebe64c3
Patrice <ppetit@wynd.eu>
2018-05-07 11:15:50 +0200

    Core : Fix lint

--- app/routes.py
+++ app/routes.py
@@ -20,11 +20,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")exit
+            flash("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        
         return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
