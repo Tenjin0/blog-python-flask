502c58688c3049b818ce5aba96b55ba15df524a8
Patrice <ppetit@wynd.eu>
2018-05-07 11:15:00 +0200

    Login : Add login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user
 from app.models import User
@@ -19,7 +19,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.username.data):
+        if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
