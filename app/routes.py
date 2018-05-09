2461119a116034fb38ba3b9b29449f41e21bb436
Patrice <ppetit@wynd.eu>
2018-05-09 10:31:18 +0200

    Profile : Add profile

--- app/routes.py
+++ app/routes.py
@@ -53,3 +53,14 @@ def register():
         flash('Congratulations, you are now registered')
         return redirect(url_for('login'))
     return render_template('register.html', title="Register", form=form)
+
+
+@app.route('/username/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
