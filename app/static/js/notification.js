fb44f341a53460d9c5beb833c68797231c09fcd8
Patrice <ppetit@wynd.eu>
2018-05-30 09:46:29 +0200

    Notifications: fix

--- app/static/js/notification.js
+++ app/static/js/notification.js
@@ -1,25 +1,28 @@
 (function(){
     function set_message_count(n) {
         document.getElementById('message_count').innerHTML = n;
-        document.getElementById('set_message_count').style.visibility, n ? "visible" : 'hidden'
+        document.getElementById('message_count').style.visibility, n ? "visible" : 'hidden'
     }
     window.onload = function() {
         var since = 0;
         this.setInterval(function(){
-            fetch("/notifictions", {
+            fetch("/notifications?since=" + since, {
                 method: "get",
                 credentials: "same-origin"
+            }).then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
             }).then((notifications) => {
                 for (var i = 0; i < notifications.length; i++) {
-                    if (notifications[i].name = 'unread_message_count') {
+                    if (notifications[i].name === 'unread_message_count') {
                         set_message_count(notifications[i].data)
                     }
                     since = notifications[i].timestamp
                 }
-                
             }).catch(() => {
 
             })
-        }, 1000)
+        }, 10000)
     }
 }())
