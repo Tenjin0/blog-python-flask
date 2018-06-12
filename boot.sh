f0d29c4d5367819af551b39522535e39f83b67bc
Patrice <ppetit@wynd.eu>
2018-06-12 17:57:48 +0200

    Docker: comment some lines

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source $(pipenv --venv)/bin/activate
 flask db upgrade
-flask translate compile
+# flask translate compile
 flask run
 # exec gunicorn -b :5000 --access-logfile - --error-logfile - myblog:app
