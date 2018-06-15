cc294c19526ce01f178d664d9987d6498c31c171
Patrice <ppetit@wynd.eu>
2018-06-15 10:25:18 +0200

    Docker : issue with web service database not available

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# source $(pipenv --venv)/bin/activate
+source $(pipenv --venv)/bin/activate
 flask db upgrade
-# flask translate compile
+flask translate compile
 flask run --host=0.0.0.0
 # exec gunicorn -b :5000 --access-logfile - --error-logfile - myblog:app
